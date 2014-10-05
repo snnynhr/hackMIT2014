2d8aac06321c93e610cd9ca46eb37ba33467c2eb
xzhangpeijin <xzhangpeijin@gmail.com>
2014-10-05 01:05:23 -0400

    Update jar package

--- MuSc/src/org/expee/MakeRandomURL.java
+++ MuSc/src/org/expee/MakeRandomURL.java
@@ -15,44 +15,44 @@ import java.util.Random;
  */
 public class MakeRandomURL
 {
-	private ArrayList<String> readData(String file) throws IOException {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				getClass().getResourceAsStream("data" + File.separator + file)));
-		
-		String nextline;
-		while ((nextline = br.readLine()) != null) {
-			result.add(nextline);
-		}
-		
-		return result;
-	}
-	
-	private void printRandomURL(ArrayList<String> adjectives, ArrayList<String> nouns) {
-		Random random = new Random(System.currentTimeMillis());
-		
-		int firstadj = random.nextInt(adjectives.size());
-		int secondadj = random.nextInt(adjectives.size());
-		int noun = random.nextInt(adjectives.size());
-		
-		System.out.println(capitalize(adjectives.get(firstadj)) + 
-											 capitalize(adjectives.get(secondadj)) + 
-											 capitalize(nouns.get(noun)));
-	}
-	
-	private String capitalize(String input) {
-		return (Character.toUpperCase(input.charAt(0)) + input.substring(1)).trim();
-	}
-	
-	public MakeRandomURL() throws IOException {
-	  ArrayList<String> adjectives = readData("adjectives.txt");
-	  ArrayList<String> nouns = readData("nouns.txt");
-		printRandomURL(adjectives, nouns);
-	}
-	
-	public static void main(String[] args) throws IOException
-	{
-		new MakeRandomURL();
-	}
+  private ArrayList<String> readData(String file) throws IOException {
+    ArrayList<String> result = new ArrayList<String>();
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(
+        getClass().getResourceAsStream("data" + File.separator + file)));
+    
+    String nextline;
+    while ((nextline = br.readLine()) != null) {
+      result.add(nextline);
+    }
+    
+    return result;
+  }
+  
+  private void printRandomURL(ArrayList<String> adjectives, ArrayList<String> nouns) {
+    Random random = new Random(System.currentTimeMillis());
+    
+    int firstadj = random.nextInt(adjectives.size());
+    int secondadj = random.nextInt(adjectives.size());
+    int noun = random.nextInt(adjectives.size());
+    
+    System.out.println(capitalize(adjectives.get(firstadj)) + 
+                       capitalize(adjectives.get(secondadj)) + 
+                       capitalize(nouns.get(noun)));
+  }
+  
+  private String capitalize(String input) {
+    return (Character.toUpperCase(input.charAt(0)) + input.substring(1)).trim();
+  }
+  
+  public MakeRandomURL() throws IOException {
+    ArrayList<String> adjectives = readData("adjectives.txt");
+    ArrayList<String> nouns = readData("nouns.txt");
+    printRandomURL(adjectives, nouns);
+  }
+  
+  public static void main(String[] args) throws IOException
+  {
+    new MakeRandomURL();
+  }
 }
