762d4b1040bf4b2265c8080d5957b180d7b3c2cf
AnanyaKumar <skywalker94@gmail.com>
2014-10-05 00:36:35 -0400

    Finished model.js

--- model.js
+++ model.js
@@ -1,1 +1,34 @@
-exports.lol = 5;
+// Array of rooms.
+
+exports.rooms = {};
+
+// Room class.
+
+exports.Room = function (numRows, numColumns)
+{
+  this.rows = numRows;
+  this.cols = numColumns;
+  this.sockets = {};
+  for (var i = 0; i < numRows; i++) {
+    (this.sockets).push([]);
+    for (var j = 0; j < numColumns; j++) {
+      (this.sockets[i]).push(undefined);
+    }
+  }
+  this.imagePath = undefined;
+  this.fileUploaded = false;
+  this.numSocketsChosen = 0;
+};
+
+exports.Room.prototype.setImagePath = function (path)
+{
+  this.imagePath = path;
+};
+
+exports.Room.prototype.addSocket = function (row, col, socket)
+{
+  if (!this.sockets[row][col]) {
+    this.numSocketsChosen += 1;
+  }
+  this.sockets[row][col] = socket;
+};
