569bbaa06d241ded512b04684b76d1b8dff85fe2
Sunny Nahar <sunnyn@cmu.edu>
2014-10-05 01:37:22 -0400

    bugfix

--- static/host.js
+++ static/host.js
@@ -1,6 +1,14 @@
 function exec() {
 	document.getElementById('submit').addEventListener('click',function()
 		{
+			$.post("makeroom",
+			  {
+			    rows:document.getElementById("h").value,
+			    cols:document.getElementById("w").value
+			  },
+			  function(data,status){
+			    alert("Data: " + data + "\nStatus: " + status);
+			  });
 			//var xmlHttp = null;
 		    //xmlHttp = new XMLHttpRequest();
 		    console.log(document.getElementById("file").value);
