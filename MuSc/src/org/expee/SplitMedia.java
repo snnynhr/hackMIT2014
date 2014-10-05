c7ee1131d0fd3d416e6c20656b0e236a95b14185
xzhangpeijin <xzhangpeijin@gmail.com>
2014-10-04 23:51:56 -0400

    Add image splitting functionality

--- MuSc/src/org/expee/SplitMedia.java
+++ MuSc/src/org/expee/SplitMedia.java
@@ -1,5 +1,7 @@
 package org.expee;
 
+import java.io.IOException;
+
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
@@ -16,22 +18,22 @@ import org.expee.splitters.VideoSplitter;
 public class SplitMedia {
 	
 	/**
-	 * Creates options for option parsing by CLI.
-	 * Options: -p Whether or not
+	 * Creates options for option parsing by CLI
+	 * 
 	 * @return Options object for parsing
 	 */
 	public static Options makeOptions() {
 		Options options = new Options();
 		
 		// Required options
-		options.addOption("p", "image", false, "Whether or not the media is an image");
+		options.addOption("i", "image", false, "Whether or not the media is an image");
 		options.addOption("v", "video", false, "Whether or not the media is a video");
-		options.addOption("i", "input", true, "Path to input file");
+		options.addOption("f", "file", true, "Path to input file (if img must be jpg/png)");
 		options.addOption("n", "num-screens", true, "Number of screens");
 		
 		// Optional options
-		options.addOption("a", "aspect-ratio", true, "Aspect ratio of the screens");
-		options.addOption("d", "distribution", true, "Distribution of screens (rows x columns)");
+		options.addOption("a", "aspect-ratio", true, "Aspect ratio of the screens (width:height)");
+		options.addOption("d", "distribution", true, "Distribution of screens (columns:rows)");
 		
 		return options;
 	}
@@ -40,27 +42,30 @@ public class SplitMedia {
 	 * Main function for splitting videos 
 	 * @param args Command line arguments
 	 * @throws ParseException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws Exception {
 		CommandLine cmd = new BasicParser().parse(makeOptions(), args);
-		if (!cmd.hasOption("input")) {
-			throw new IllegalArgumentException("Input argument must be specified");
+		if (!cmd.hasOption("file")) {
+			throw new IllegalArgumentException("File argument must be specified");
 		} else if (!cmd.hasOption("num-screens")) {
 			throw new IllegalArgumentException("Number of screens must be specified");
 		} else if (!(cmd.hasOption("image") ^ cmd.hasOption("video"))) {
 			throw new IllegalArgumentException("One of image or video argument must be specified");
-		}
+		} 
 		
-		// valid command lines arguments
+		// All required command line arguments are present
 		
 		AbstractSplitter splitter = null;
 		if (cmd.hasOption("image")) {
-			splitter = new ImageSplitter(cmd.getOptionValue("input"), 
+			splitter = new ImageSplitter(cmd.getOptionValue("file"),
+					cmd.getOptionValue("num-screens"),
 					cmd.getOptionValue("aspect-ratio", "16:9"),
 					cmd.getOptionValue("distribution"));
 		}
 		if (cmd.hasOption("video")) {
-			splitter = new VideoSplitter(cmd.getOptionValue("input"), 
+			splitter = new VideoSplitter(cmd.getOptionValue("file"),
+					cmd.getOptionValue("num-screens"),
 				  cmd.getOptionValue("aspect-ratio", "16:9"),
 				  cmd.getOptionValue("distribution"));
 		}
--- MuSc/src/org/expee/splitters/VideoSplitter.java
+++ MuSc/src/org/expee/splitters/VideoSplitter.java
@@ -1,16 +1,25 @@
 package org.expee.splitters;
 
+/**
+ * Given an image file, splits it into N parts where each part represents one section of the image
+ * Each part is guaranteed to have the same bitrate and play at the same speed
+ * 
+ * @author Peijin Zhang
+ */
 public class VideoSplitter extends AbstractSplitter
 {
-
-	public VideoSplitter(String input, String aspect, String distribution) {
-		super(input, aspect, distribution);
+	public VideoSplitter(String input, String num, String aspect, String distribution) {
+		super(input, num, aspect, distribution);
 	}
 	
-	public int[] getResolution(String input) {
+	public int[] getResolution() {
 		int[] resolution = new int[2];
 		
 		return resolution;
+	}
+	
+	public void scaleInput() {
+		
 	}
 	
 	public void splitFile() {
