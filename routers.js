7ec52015ef80aaef3f41154b07e927da1276e614
AnanyaKumar <skywalker94@gmail.com>
2014-10-05 02:12:05 -0400

    Changed backend a flow, updated css references

--- routers.js
+++ routers.js
@@ -14,26 +14,45 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname,'title.html'));
 });
 
-// Host function
+app.get('/host', hostHandler);
 
-app.get('/host', function(req, res){
+app.get('/host/:room', function(req, res){
   res.sendFile(path.join(__dirname,'host.html'));
 });
-
-// Join function
 
 app.get('/join', function(req, res){
   res.sendFile(path.join(__dirname,'join.html'));
 });
 
-app.get('/upload', uploadFile);
+app.get('/join/:room', joinHandler);
 
-app.get('/upload', uploadFile);
+app.post('/upload', uploadFile);
 
-// Core files
+app.post('/makeroom/:room', makeRoom);
+
+// Core functions
+
+function hostHandler(req, res) {
+  var roomName = getRoomName();
+  model.rooms[roomName] = new model.Room();
+  res.statusCode = 302; 
+  res.setHeader('Location', '/host/' + roomName);
+  res.end();
+}
 
 function makeRoom(req, res) {
+  var roomName = req.param('room');
+  var numRows = req.param('rows');
+  var numCols = req.param('cols');
 
+  model.rooms[roomName].makeRoom(numRows, numCols);
+  res.statusCode = 302; 
+  res.setHeader('Location', '/join/' + roomName);
+  res.end();
+}
+
+function joinHandler(req, res) {
+  
 }
 
 function uploadFile(req, res) {
@@ -41,7 +60,7 @@ function uploadFile(req, res) {
 }
 
 function updateGridPositions(req, res) {
-
+  
 }
 
 function tryDistributeImage() {
@@ -50,6 +69,12 @@ function tryDistributeImage() {
 
 function distributeImage() {
 
+}
+
+// Utilities
+
+function getRoomName() {
+  return Math.round(Math.random()*1000);
 }
 
 function fragmentImage() {
