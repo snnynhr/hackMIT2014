b3099a97f461a0819f0b4a639651d7cf7e8c89ef
xzhangpeijin <xzhangpeijin@gmail.com>
2014-10-05 04:54:29 -0400

    Finished java code

---
+++ MuSc/src/org/expee/musc/MakeRandomURL.java
@@ -0,0 +1,58 @@
+package org.expee.musc;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Every time this is called, will simply print out a random string to console that consists
+ * of adjective + adjective + noun. Used for generation of random URLs
+ * 
+ * @author Peijin Zhang
+ */
+public class MakeRandomURL
+{
+  private ArrayList<String> readData(String file) throws IOException {
+    ArrayList<String> result = new ArrayList<String>();
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(
+        getClass().getResourceAsStream("data" + File.separator + file)));
+    
+    String nextline;
+    while ((nextline = br.readLine()) != null) {
+      result.add(nextline);
+    }
+    
+    return result;
+  }
+  
+  private void printRandomURL(ArrayList<String> adjectives, ArrayList<String> nouns) {
+    Random random = new Random(System.currentTimeMillis());
+    
+    int firstadj = random.nextInt(adjectives.size());
+    int secondadj = random.nextInt(adjectives.size());
+    int noun = random.nextInt(adjectives.size());
+    
+    System.out.println(capitalize(adjectives.get(firstadj)) + 
+                       capitalize(adjectives.get(secondadj)) + 
+                       capitalize(nouns.get(noun)));
+  }
+  
+  private String capitalize(String input) {
+    return (Character.toUpperCase(input.charAt(0)) + input.substring(1)).trim();
+  }
+  
+  public MakeRandomURL() throws IOException {
+    ArrayList<String> adjectives = readData("adjectives.txt");
+    ArrayList<String> nouns = readData("nouns.txt");
+    printRandomURL(adjectives, nouns);
+  }
+  
+  public static void main(String[] args) throws IOException
+  {
+    new MakeRandomURL();
+  }
+}
---
+++ MuSc/src/org/expee/musc/SplitMedia.java
@@ -0,0 +1,75 @@
+package org.expee.musc;
+
+import java.io.IOException;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.expee.musc.splitters.AbstractSplitter;
+import org.expee.musc.splitters.ImageSplitter;
+import org.expee.musc.splitters.VideoSplitter;
+
+/** 
+ * Main class for doing the media splitting with MuSc
+ *   call with "java -cp MuSc.jar org.expee.musc.SplitMedia -i -f image.jpg"
+ *   
+ * @author Peijin Zhang
+ */
+public class SplitMedia {
+  
+  /**
+   * Creates options for option parsing by CLI
+   * 
+   * @return Options object for parsing
+   */
+  public static Options makeOptions() {
+    Options options = new Options();
+    
+    // Required options
+    options.addOption("i", "image", false, "Whether or not the media is an image");
+    options.addOption("v", "video", false, "Whether or not the media is a video");
+    options.addOption("f", "file", true, "Path to input file (if img must be jpg/png)");
+    options.addOption("n", "num-screens", true, "Number of screens");
+    
+    // Optional options
+    options.addOption("a", "aspect-ratio", true, "Aspect ratio of the screens (width:height)");
+    options.addOption("d", "distribution", true, "Distribution of screens (columns:rows)");
+    
+    return options;
+  }
+  
+  /**
+   * Main function for splitting videos 
+   * @param args Command line arguments
+   * @throws ParseException 
+   * @throws IOException 
+   */
+  public static void main(String[] args) throws Exception {
+    CommandLine cmd = new BasicParser().parse(makeOptions(), args);
+    if (!cmd.hasOption("file")) {
+      throw new IllegalArgumentException("File argument must be specified");
+    } else if (!cmd.hasOption("num-screens")) {
+      throw new IllegalArgumentException("Number of screens must be specified");
+    } else if (!(cmd.hasOption("image") ^ cmd.hasOption("video"))) {
+      throw new IllegalArgumentException("One of image or video argument must be specified");
+    } 
+    
+    // All required command line arguments are present
+    
+    AbstractSplitter splitter = null;
+    if (cmd.hasOption("image")) {
+      splitter = new ImageSplitter(cmd.getOptionValue("file"),
+          cmd.getOptionValue("num-screens"),
+          cmd.getOptionValue("aspect-ratio", "16:9"),
+          cmd.getOptionValue("distribution"));
+    }
+    if (cmd.hasOption("video")) {
+      splitter = new VideoSplitter(cmd.getOptionValue("file"),
+          cmd.getOptionValue("num-screens"),
+          cmd.getOptionValue("aspect-ratio", "16:9"),
+          cmd.getOptionValue("distribution"));
+    }
+    splitter.splitFile();
+  }
+}
