7fab20a6ba56febc6c66f6bb0e0c481e75c307c5
Sunny Nahar <sunnyn@cmu.edu>
2014-10-05 01:01:01 -0400

    js fix

--- static/js/grayscale.js
+++ static/js/grayscale.js
@@ -6,10 +6,13 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    if(!$(".navbar"))
+    {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
     }
 });
 
