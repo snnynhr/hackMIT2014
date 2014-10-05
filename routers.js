b52e5294c6d101272f64ec18221d50d4c4908e6e
AnanyaKumar <skywalker94@gmail.com>
2014-10-04 22:40:46 -0400

    Changed routers.js

--- routers.js
+++ routers.js
@@ -1,9 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var path = require('path');
+
+// Routers
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(path.join(__dirname,'title.html'));
 });
+
+
+// Host function
+
+
+
+// Join function
+
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
